415c2819d9d0d87fc333f4b69244eb1f3ff43cd3
Ragavendiran <36047046+RAGANITHI@users.noreply.github.com>
2021-08-15 11:58:33 +0530

    updated Source code on EJS track 42

--- publish_1.js
+++ publish_1.js
@@ -6,7 +6,7 @@ var express = require("express");
 var geoip = require('geoip-lite');
 const rp = require('request-promise');
 const cheerio = require('cheerio');
-
+var resultData;
 //use the application off of express.
 var app = express();
 var selfip;
@@ -14,6 +14,12 @@ app.use(express.static("public"));
 app.get("/", function (request, response){
     response.sendFile("./Testindex.html", { root: __dirname });
 });
+app.set("view engine", "ejs");
+
+// EDIT YOUR LOCATION HERE
+app.set('views', 'G:\\Downloads\\Final\\Final\\REACT\\views');
+
+
 app.get("/getemail", function (request, response){
     var firstname = request.query.firstname;
     var ipd,lt,lg,dn;
@@ -50,8 +56,17 @@ app.get("/getemail", function (request, response){
       .catch(function(err){
         //handle error
       });
+      subs()
   
 });
+
+app.get("/result", (req, res) => {
+    console.log(resultData.site)
+    var site = resultData.site
+    res.render("result", {name: resultData});
+    
+})
+
 app.listen(3000);
  
  console.log("Something awesome to happen at http://localhost:3000");
@@ -64,6 +79,7 @@ const client = new StreamrClient({
 //publishData();
 var l=1;
 async function publishData(lat,lng,ip,sitename,dnsprop){
+
     var streamrData;
     streamrData={
         'ip':ip,
@@ -75,4 +91,16 @@ async function publishData(lat,lng,ip,sitename,dnsprop){
 console.log(streamrData);
 client.publish('0xe01b4f574ff0b2f5393a4a934e473db2c894ea45/webstream_userdata', streamrData);
 console.log("Streamr data push Done");
+}
+
+async function subs() {
+
+const sub = await client.subscribe({
+    stream: '0xe01b4f574ff0b2f5393a4a934e473db2c894ea45/webstream_userdata',
+    partition: 0,
+}, (msg, metadata) => {
+    //console.log( "subs da", msg.dns, msg.site, msg.lat, msg.lng)
+    resultData = msg
+    console.log("subscribed")
+})
 }
